b6ed6f95bb90ac237f1bf204e46f8fdd1ffa6919
Bharadwaj Raghavan <bharad6@gmail.com>
2015-11-01 14:58:42 -0800

    Got the GPS and SD Card Libraries loaded

--- main.cpp
+++ main.cpp
@@ -1,120 +1,115 @@
+/*
+###############################
+mbed nucleo l152re sd and gps test
+using tinygps v13 and sdfilesystem libraries.
+last change: 10/12/2015
+status:
+    - gps working
+    - sd logging functional
+
+###############################
+*/
+
 #include "mbed.h"
-#include "IridiumSBD.h"
+#include "SDFileSystem.h"
+#include "TinyGPS.h"
 
-DigitalOut myled(LED1);
-Serial diagSerial(USBTX, USBRX, "diagnostic");
-Serial nss(D8, D2, "isbdSerial");
-IridiumSBD isbd(nss, NC);
+//Serial gps_ser(PA_9,PA_10); //serial to gps, 9600 baud. D8 <-> TX , D2 <-> RX
+Serial gps_ser(D8,D2); //serial to gps, 9600 baud. D8 <-> TX , D2 <-> RX
+SDFileSystem sd(SPI_MOSI, SPI_MISO, SPI_SCK, D9, "sd"); // the pinout on the mbed Cool Components workshop board / mosi, miso, sclk, cs
+///SPI_CS
+TinyGPS gps;
 
-bool ISBDCallback()
-{
-   myled = (millis() / 1000) % 2 == 1 ? 1 : 0;
-   return true;
-}
+//function definitions
+static void print_date(FILE *fp, TinyGPS &gps);
 
 int main() {
-    set_time(1256729737);
-    startMillis();
-    //IridiumSBD Setup
-    int signalQuality = -1;
-
-    diagSerial.baud(115200); //Serial -> mbed: Serial pc(tx, rx)
-    diagSerial.printf("Hello Iridium\n");
-    diagSerial.printf("hello diagnostic serial\n");
-    nss.baud(19200);
-
-    diagSerial.printf("nss baud init\n");
+    printf("Hello World!\n");   
+ 
+    mkdir("/sd/mydir2", 0777);
     
-    isbd.attachConsole(diagSerial); // for diagnostic purposes
-    isbd.setPowerProfile(1); // 0 for direct connect, 1 for usb connect: only changes the waiting interval in internalSendAndReceive
-    diagSerial.printf("isbd setPowerProfile\n");
-    isbd.begin();
-    diagSerial.printf("isbd.begin()\n");
+    FILE *fp = fopen("/sd/filetest.txt", "a");
+    if(fp == NULL) {
+        error("Could not open file for write\n");
+    }
+    fprintf(fp, "Hello fun SD Card World!"); 
+    printf("Goodbye World!\n");
+    fclose(fp); 
     
-    // gets the signal quality by reference
-    int err = isbd.getSignalQuality(signalQuality); 
-    diagSerial.printf("isbd getSignalQuality\n %d", err);
-    if (err != 0)
+    gps_ser.baud(9600);
+    printf("Simple TinyGPS library v. %i\n",TinyGPS::library_version());
+    while(1)
     {
-      diagSerial.printf("SignalQuality failed: error ");
-      diagSerial.printf("%d\n", err);
-      return 1;
-    }
+        bool newData = false;
+        unsigned long chars;
+        unsigned short sentences, failed;
+
+        // For one second we parse GPS data and report some key values
+        for (unsigned long start = time(NULL); time(NULL) - start < 1;)
+        {
+            while (gps_ser.readable())
+            {
+                char c = gps_ser.getc();
+                //pc.printf("%c",c); // uncomment this line if you want to see the GPS data flowing
+                if (gps.encode(c)) // Did a new valid sentence come in?
+                newData = true;
+            }
+        }
+        FILE *fp = fopen("/sd/filetest.txt", "a");
+        if (newData)
+        {
+            float flat, flon;
+            unsigned long age;
+            gps.f_get_position(&flat, &flon, &age);
+            print_date(fp, gps);
+            printf("LAT=");
+            printf("%.6f",flat);
+            fprintf(fp,"LAT= %.6f",flat);
+            printf(" LON=");
+            printf("%.6f",flon);
+            fprintf(fp," LON= %.6f",flon);
+            printf(" ALT=");
+            printf("%.6f",gps.f_altitude());
+            fprintf(fp," ALT= %.6f",gps.f_altitude());
+            printf(" PREC= %i", gps.hdop());
+            fprintf(fp," PREC= %i", gps.hdop());
+            
+        }
+  
+        gps.stats(&chars, &sentences, &failed);
+        printf(" CHARS=");
+        printf("%u",chars);
+        fprintf(fp," CHARS = %u",chars);
+        
+        printf(" SENTENCES=");
+        printf("%u",sentences);
+        fprintf(fp," SENTENCES = %u",sentences);
+        
+        printf(" CSUM ERR=");
+        printf("%u\n",failed);
+        fprintf(fp," CSUM ERR = %u\n",failed);
+        fclose(fp);
+        if (chars == 0) printf("** No characters received from GPS: check wiring **\n");
+    }   
     
-    diagSerial.printf("Signal quality is ");
-    diagSerial.printf("%d\n", signalQuality);
-    
-    err = isbd.sendSBDText("Hello, world!");
-    if (err != 0)
-    {
-      diagSerial.printf("sendSBDText failed: error ");
-      diagSerial.printf("%d\n",err);
-      return 1;
-    }
-
-    diagSerial.printf("Hey, it worked!\n");
-    diagSerial.printf("Messages left: ");
-    diagSerial.printf("%d\n", isbd.getWaitingMessageCount());
-
-    while(1) {
-        time_t seconds = time(NULL);
-
-        diagSerial.printf("Time as a basic string = %s", ctime(&seconds));
-
-        myled = !myled;      
-        wait(1);
-    }
-    return 0;
 }
 
-/*
-static const int ledPin = 13;
-
-void setup()
+static void print_date(FILE *fp, TinyGPS &gps)
 {
-  int signalQuality = -1;
-
-  pinMode(ledPin, OUTPUT);
-
-  Serial.begin(115200); # Serial -> mbed: Serial pc(tx, rx)
-  nss.begin(19200);
-
-  isbd.attachConsole(Serial); # this serial port is for diagnostic purposes
-  isbd.setPowerProfile(1); # 0 for direct connect, 1 for usb connect: only changes the waiting interval in internalSendAndReceive
-  isbd.begin();
-
-  int err = isbd.getSignalQuality(signalQuality); # gets the signal quality by reference
-  if (err != 0)
-  {
-    Serial.print("SignalQuality failed: error ");
-    Serial.println(err);
-    return;
+  int year;
+  byte month, day, hour, minute, second, hundredths;
+  unsigned long age;
+  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
+  if (age == TinyGPS::GPS_INVALID_AGE){
+    printf("********** ******** ");
+    fprintf(fp,"********** ******** ");
   }
-
-  Serial.print("Signal quality is ");
-  Serial.println(signalQuality);
-
-  err = isbd.sendSBDText("Hello, world!");
-  if (err != 0)
+  else
   {
-    Serial.print("sendSBDText failed: error ");
-    Serial.println(err);
-    return;
+    char sz[32];
+    sprintf(sz, "DATE&TIME = %02d/%02d/%02d %02d:%02d:%02d ",
+        month, day, year, hour, minute, second);
+    printf(sz);
+    fprintf(fp,sz);
   }
-
-  Serial.println("Hey, it worked!");
-  Serial.print("Messages left: ");
-  Serial.println(isbd.getWaitingMessageCount());
 }
-
-void loop()
-{
-   digitalWrite(ledPin, HIGH);
-}
-
-bool ISBDCallback()
-{
-   digitalWrite(ledPin, (millis() / 1000) % 2 == 1 ? HIGH : LOW);
-   return true;
-}
-*/
